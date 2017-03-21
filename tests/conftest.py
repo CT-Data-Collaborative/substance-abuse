017d33ce31af9db577a1cfa239e6c7bc3866cfd9
jennadaly <jenna.m.daly@gmail.com>
2017-03-21 13:00:26 -0400

    Updates to tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,12 @@
 import pytest
 import datapackage
 
-
-
 @pytest.fixture
-def towns():
-    """Load our town list datapackage"""
-    dp = datapackage.DataPackage('https://raw.githubusercontent.com/CT-Data-Collaborative/ct-town-list/master/datapackage.json')
-    return dp.resources[0].data
-
-
+def towns_and_counties():
+    towns = datapackage.DataPackage(
+        'https://raw.githubusercontent.com/CT-Data-Collaborative/ct-town-list/master/datapackage.json')
+    counties = datapackage.DataPackage(
+        'https://raw.githubusercontent.com/CT-Data-Collaborative/ct-county-list/master/datapackage.json')
+    town_name_list = [t['Town'] for t in towns.resources[0].data]
+    county_name_list = [c['County'] for c in counties.resources[0].data]
+    return town_name_list + county_name_list
